f9f8670ae9d968b5ff92359cfe957a7010b629f4
erhei <ljerhei@163.com>
2019-12-18 15:58:22 +0800

    Add files via upload

---
+++ contains-duplicate.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <ctime>
+#include <vector>
+#include <stack>
+#include <cmath>
+#include <map>
+#include <set>
+#include <unordered_map>
+#include <unordered_set>
+#include <string>
+#include <queue>
+#include <algorithm>
+#include "solution.h"
+using std::cin;
+using std::cout;
+using std::endl;
+using std::srand;
+using std::rand;
+using std::stack;
+using std::vector;
+using std::pow;
+using std::string;
+using std::strlen;
+using std::map;
+using std::set;
+using std::queue;
+using std::pair;
+using std::string;
+using std::unordered_map;
+using std::sort;
+using std::unordered_set;
+
+inline int max(int val1,int val2){
+	return val1>val2?val1:val2;
+}
+
+int main(){
+	vector<int> nums;
+	nums.push_back(1);
+	nums.push_back(2);
+	nums.push_back(3);
+	nums.push_back(1);
+	unordered_set<int> heap;
+	bool result=false;
+	int size=nums.size();
+	for(int i=0;i<size;++i){
+		if(heap.find(nums[i])!=heap.end()) {
+			result=true;
+			break;
+		}else heap.insert(nums[i]);
+	}
+	cout<<result;
+	cin.get();
+	cin.get();
+	return 0;
+}
